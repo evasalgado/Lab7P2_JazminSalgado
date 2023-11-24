fd03b5b7a6e641eadace50341c50b6f79cd7d404
evasalgado <imviarsha@gmail.com>
2023-11-24 13:26:34 -0600

    clase productos agregada

--- Lab7P2_JazminSalgado/src/lab7p2_jazminsalgado/restaurante.java
+++ Lab7P2_JazminSalgado/src/lab7p2_jazminsalgado/restaurante.java
@@ -4,10 +4,53 @@
  */
 package lab7p2_jazminsalgado;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author evaja
  */
 public class restaurante {
+    private String nombre;
+    private String ubicacion;
+    private ArrayList<String> productos=new ArrayList<>();
+
+    public restaurante() {
+    }
+
+    public restaurante(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public ArrayList<String> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<String> productos) {
+        this.productos = productos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre ;
+    }
+    
     
 }
